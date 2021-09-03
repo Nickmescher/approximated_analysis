d053c9b2a2a1838841b33a9c90dc2d1d45622462
Adenisova <adenisova@intetech.ru>
2021-09-03 17:55:11 +0300

    approx

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cufflinks as cf
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-# [4812:5370] для 501 скважины 'Time': p['Время (UTC)'],
+# [4812:5370] для 501 скважины 'Time': p['Время (UTC)'], p['Qnew'] = p['Qnew'].dropna(axis='index', how='any')
 
 fig = go.Figure()
 fig = make_subplots(rows=3, cols=1)
@@ -14,12 +14,11 @@ fig2 = go.Figure()
 fig2 = make_subplots(rows=1, cols=1)
 fig3 = go.Figure()
 fig3 = make_subplots(rows=2, cols=1)
-fig4 = go.Figure()
-fig4 = make_subplots(rows=1, cols=1)
 
 p = pd.read_csv(r"C:\Users\Skytech2028\Desktop\\nick\boreholeAnalisys\501.csv", encoding='utf8', sep=';',
                 decimal=',', parse_dates=['Время (UTC)'],
                 usecols=['Время (UTC)', 'Скв501_Скв501.%откр', 'Скв501_Скв501.Qгаз'])
+tempdf = pd.DataFrame()
 
 for n in enumerate(p['Скв501_Скв501.%откр']):
     if n[1] <= 0 or n[1] > 100:
@@ -33,15 +32,24 @@ for n in enumerate(p['Скв501_Скв501.Qгаз']):
 
 fig2 = px.scatter(x=p["Время (UTC)"],
                   y=p["Скв501_Скв501.Qгаз"],
-                  trendline="rolling", trendline_options=dict(function="median", window=100))
+                  trendline="rolling", trendline_options=dict(function="median", window=100),
+                  trendline_color_override="red")
 
 p['diff'] = p['Скв501_Скв501.Qгаз'].diff()
-p['Qnew'] = fig2.data[1].y
+tempdf['Qnew'] = fig2.data[1].y
+
+tempdf.drop(tempdf.index[1:50], inplace=True)
+tempdf.reset_index(inplace=True)
+
+
+p['Qnew'] = tempdf['Qnew']
 p['diff2'] = p['Qnew'].diff()
 p['diff3'] = p['Скв501_Скв501.%откр'].diff()
 
 df = pd.DataFrame({'Q': p['Qnew'],
-                   'Perc': p['Скв501_Скв501.%откр']})
+                   'Perc': p['Скв501_Скв501.%откр'],
+                   'Q_Diff': p['diff2'],
+                   'Perc_Diff': p['diff3']})
 
 scaler = MinMaxScaler()
 
@@ -67,36 +75,36 @@ fig.append_trace(go.Scatter(
     name="approximated",
 ), row=3, col=1)
 
-fig.update_layout(title_text="Скважина 501")
-
 fig3.append_trace(go.Scatter(
-    x=p["Время (UTC)"],
-    y=p["diff3"],
-    name='PercentageDiff'
-), row=1, col=1)
-
-fig3.append_trace(go.Scatter(
-    x=p["Время (UTC)"],
-    y=p["diff2"],
-    name='ApproxedDiff'
-), row=2, col=1)
-
-fig4.append_trace(go.Scatter(
     x=p['Время (UTC)'],
-    y=df_scaled['Perc'],
+    y=normalized_df['Perc'],
     name='Percentage'
 ), row=1, col=1)
 
-fig4.append_trace(go.Scatter(
+fig3.append_trace(go.Scatter(
     x=p['Время (UTC)'],
-    y=df_scaled['Q'],
+    y=normalized_df['Q'],
     name='Q'
 ), row=1, col=1)
 
+fig3.append_trace(go.Scatter(
+    x=p['Время (UTC)'],
+    y=normalized_df['Q_Diff'],
+    name='Q_Diff'
+), row=2, col=1)
+
+fig3.append_trace(go.Scatter(
+    x=p['Время (UTC)'],
+    y=normalized_df['Perc_Diff'],
+    name='Perc_Diff'
+), row=2, col=1)
+
+fig.update_layout(title_text="Скважина 501")
+fig2.update_layout(title_text="Скважина 501")
+fig3.update_layout(title_text="Скважина 501")
+
 fig.show()
-fig2.show()
 fig3.show()
-fig4.show()
 
 print(df)
 print(df_scaled)
