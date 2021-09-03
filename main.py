882951f20f0b5fba7d99f9b05cc266cfcec0bb3b
Adenisova <adenisova@intetech.ru>
2021-09-03 15:38:24 +0300

    approx

--- main.py
+++ main.py
@@ -4,30 +4,30 @@ from plotly.subplots import make_subplots
 import plotly.express as px
 import cufflinks as cf
 import numpy as np
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
-# [4812:5370] для 501 скважины
+# [4812:5370] для 501 скважины 'Time': p['Время (UTC)'],
 
 fig = go.Figure()
 fig = make_subplots(rows=3, cols=1)
-cf.go_offline()
 fig2 = go.Figure()
 fig2 = make_subplots(rows=1, cols=1)
 fig3 = go.Figure()
 fig3 = make_subplots(rows=2, cols=1)
+fig4 = go.Figure()
+fig4 = make_subplots(rows=1, cols=1)
 
 p = pd.read_csv(r"C:\Users\Skytech2028\Desktop\\nick\boreholeAnalisys\501.csv", encoding='utf8', sep=';',
-                decimal=',', parse_dates=['Время (UTC)'])
+                decimal=',', parse_dates=['Время (UTC)'],
+                usecols=['Время (UTC)', 'Скв501_Скв501.%откр', 'Скв501_Скв501.Qгаз'])
 
 for n in enumerate(p['Скв501_Скв501.%откр']):
-    n = list(n)
-    if n[1] <= 0:
+    if n[1] <= 0 or n[1] > 100:
         p['Скв501_Скв501.%откр'][n[0]] = p['Скв501_Скв501.%откр'][n[0] - 1]
     print(n)
 
 for n in enumerate(p['Скв501_Скв501.Qгаз']):
-    n = list(n)
-    if n[1] <= 0:
+    if n[1] <= 0 or n[1] > 100:
         p['Скв501_Скв501.Qгаз'][n[0]] = p['Скв501_Скв501.Qгаз'][n[0] - 1]
     print(n)
 
@@ -36,9 +36,18 @@ fig2 = px.scatter(x=p["Время (UTC)"],
                   trendline="rolling", trendline_options=dict(function="median", window=100))
 
 p['diff'] = p['Скв501_Скв501.Qгаз'].diff()
-p['diff2'] = fig2.data[1].y
-p['diff2'] = p['diff2'].diff()
+p['Qnew'] = fig2.data[1].y
+p['diff2'] = p['Qnew'].diff()
 p['diff3'] = p['Скв501_Скв501.%откр'].diff()
+
+df = pd.DataFrame({'Q': p['Qnew'],
+                   'Perc': p['Скв501_Скв501.%откр']})
+
+scaler = MinMaxScaler()
+
+df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+normalized_df = (df - df.mean()) / df.std()
 
 fig.append_trace(go.Scatter(
     x=p["Время (UTC)"],
@@ -60,27 +69,35 @@ fig.append_trace(go.Scatter(
 
 fig.update_layout(title_text="Скважина 501")
 
-
 fig3.append_trace(go.Scatter(
     x=p["Время (UTC)"],
     y=p["diff3"],
-    name='PercentageDiff',
+    name='PercentageDiff'
 ), row=1, col=1)
 
 fig3.append_trace(go.Scatter(
     x=p["Время (UTC)"],
     y=p["diff2"],
-    name='ApproxedDiff',
+    name='ApproxedDiff'
 ), row=2, col=1)
+
+fig4.append_trace(go.Scatter(
+    x=p['Время (UTC)'],
+    y=df_scaled['Perc'],
+    name='Percentage'
+), row=1, col=1)
+
+fig4.append_trace(go.Scatter(
+    x=p['Время (UTC)'],
+    y=df_scaled['Q'],
+    name='Q'
+), row=1, col=1)
 
 fig.show()
 fig2.show()
 fig3.show()
+fig4.show()
 
-# d = preprocessing.normalize(p, axis=0)
-#
-# scaled_df = pd.DataFrame(d, columns='Время')
-# scaled_df.head()
-
-print(p["diff2"])
-print(d)
+print(df)
+print(df_scaled)
+print()
